cb88c8c580c049e17fc63b234b667aaa0f66b996
Martynas Kanapinskas <martis347@gmail.com>
2016-10-16 03:17:25 +0300

    Added support for rules of sensors and alerts if they're broken

---
+++ php/removeRule.php
@@ -0,0 +1,21 @@
+<?php
+$rule = $_REQUEST['rule'];
+
+include 'config.php';
+// Create connection
+$conn = new mysqli($db_config['servername'], $db_config['username'], $db_config['password'], $db_config['dbname']);
+
+$conn->set_charset("utf8");
+
+if ($conn->connect_error) {
+	echo "connectionError";
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "DELETE sr FROM sensor_rules AS sr JOIN authentication_token AS at ON at.token = '" . $_COOKIE['authToken'] . "' WHERE at.userId = sr.userId AND sr.rule='" . $rule . "'";
+echo $sql;
+
+$result = $conn->query($sql);
+
+$conn->close();
+?>
