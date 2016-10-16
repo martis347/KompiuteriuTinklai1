bb78c33c123c0cc502d2c8cf6752cc68b7b3b0ef
Martynas Kanapinskas <martis347@gmail.com>
2016-10-16 13:57:08 +0300

    added support for localhost and production environments simultaneously

--- php/test.php
+++ php/test.php
@@ -1,14 +1,4 @@
 <?php
-$datetime1 = date('Y-m-d h:i:s');
-$datetime2 = date('2016-10-14 10:27:52');
 
-
-$ts1 = strtotime($datetime1);
-$ts2 = strtotime($datetime2);
-
-$seconds_diff = $ts2 - $ts1;
-include 'config.php';
-
-
-echo $db_config['servername'];
+echo $_SERVER['SERVER_NAME'];
 ?>
